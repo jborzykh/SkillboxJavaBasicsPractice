55459556b23ff056cfaca8ab5ec17f1601393901
jborzykh <jborzykh@mail.ru>
2023-01-17 17:12:02 +0300

    module 5.6

--- src/main/java/Product/Product.java
+++ src/main/java/Product/Product.java
@@ -3,15 +3,20 @@ package Product;
 // создание POJO class
 
 public class Product {
-    private String name;
-    private int price;
+    private final String name;
+    private final int price;
 
-    public Product (String name, int price){
-       this.name = name;
-       this.price = price;
-    }
-    public void setName(String name) {
+    public Product(String name, int price) {
         this.name = name;
+        this.price = price;
+    }
+
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+
+    public Product setName(String name) {
+        return new Product(name, price);
     }
 
     public String getName() {
@@ -19,13 +24,24 @@ public class Product {
     }
 
 
-
     public int getPrice() {
         return price;
     }
 
-    public void setPrice(int price) {
-        this.price = price;
+    //    public void setPrice(int price) {
+//        this.price = price;
+//    }
+    public Product setPrice(int price) {
+        return new Product(name, price);
+    }
+
+    public String toString() {
+        return name + " - " + price;
+    }
+
+    public void print() {
+        String info = name + " - " + price;
+        System.out.println(info);
     }
 
 
